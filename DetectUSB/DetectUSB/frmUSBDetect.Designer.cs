d8ebcfa4f277d2a091a6482945c333c76ba50435
Tsunehiko Kurachi <tsk.rachi@gmail.com>
2022-01-06 18:49:39 +0900

    LogをListViewに出力するよう修正

--- DetectUSB/DetectUSB/frmUSBDetect.Designer.cs
+++ DetectUSB/DetectUSB/frmUSBDetect.Designer.cs
@@ -29,22 +29,36 @@
         private void InitializeComponent()
         {
             this.txtUSBInfo = new System.Windows.Forms.TextBox();
+            this.lvwUsbDevice = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // txtUSBInfo
             // 
-            this.txtUSBInfo.Location = new System.Drawing.Point(12, 80);
+            this.txtUSBInfo.Location = new System.Drawing.Point(12, 185);
             this.txtUSBInfo.Multiline = true;
             this.txtUSBInfo.Name = "txtUSBInfo";
             this.txtUSBInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtUSBInfo.Size = new System.Drawing.Size(469, 79);
             this.txtUSBInfo.TabIndex = 0;
             // 
+            // lvwUsbDevice
+            // 
+            this.lvwUsbDevice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwUsbDevice.Location = new System.Drawing.Point(12, 12);
+            this.lvwUsbDevice.MultiSelect = false;
+            this.lvwUsbDevice.Name = "lvwUsbDevice";
+            this.lvwUsbDevice.Size = new System.Drawing.Size(469, 167);
+            this.lvwUsbDevice.TabIndex = 1;
+            this.lvwUsbDevice.UseCompatibleStateImageBehavior = false;
+            this.lvwUsbDevice.View = System.Windows.Forms.View.Details;
+            // 
             // frmUSBDetect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 213);
+            this.ClientSize = new System.Drawing.Size(493, 276);
+            this.Controls.Add(this.lvwUsbDevice);
             this.Controls.Add(this.txtUSBInfo);
             this.Name = "frmUSBDetect";
             this.Text = "Form1";
@@ -56,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtUSBInfo;
+        private System.Windows.Forms.ListView lvwUsbDevice;
     }
 }
 
--- DetectUSB/DetectUSB/frmUSBDetect.cs
+++ DetectUSB/DetectUSB/frmUSBDetect.cs
@@ -22,58 +22,107 @@ namespace DetectUSB
         public frmUSBDetect()
         {
             InitializeComponent();
+
+            InitializeListView();
+
             Task.Run(() => CheckDevice());
+        }
+
+        private void InitializeListView()
+        {
+            lvwUsbDevice.View = View.Details;
+
+            ColumnHeader chTime = new ColumnHeader();
+            chTime.Text = "時刻";
+            chTime.Width = 100;
+
+            ColumnHeader chOperate = new ColumnHeader();
+            chOperate.Text = "操作";
+            chOperate.Width = 100;
+
+            ColumnHeader chDevId = new ColumnHeader();
+            chDevId.Text = "Device ID";
+            chDevId.Width = 100;
+
+            ColumnHeader chTest = new ColumnHeader();
+            chTest.Text = "Test";
+            chTest.Width = 100;
+
+            ColumnHeader chTest2 = new ColumnHeader();
+            chTest2.Text = "Test";
+            chTest2.Width = 100;
+
+            ColumnHeader[] colHeader = {
+                chTime, chOperate, chDevId, chTest, chTest2
+            };
+
+            lvwUsbDevice.Columns.AddRange(colHeader);
         }
                                                                                                                                                                                                                 
         private void CheckDevice()
         {
             var usbDevices = GetUSBDevices();
             string nowTime = DateTime.Now.ToString("HH:mm:ss.ff ");
+            Console.WriteLine("{2}:前回件数:{0},今回件数{1}", usbDevicesBefore.Count, usbDevices.Count,nowTime);
 
             if (usbDevices.Count > numBeforeUsbDevices)
             {
+                // 今回検出件数＞前回検出件数 ⇒ 追加
                 foreach( var usbDevice in usbDevices)
                 {
                     bool bExistDevice = false;
 
+                    // 前回検出したレコードと比較する
                     foreach (var usbDeviceBefore in usbDevicesBefore)
                     {
                         if (usbDevice.DeviceID == usbDeviceBefore.DeviceID)
                         {
+                            // 同一のデバイスIDが見つかれば、検索を中止する
                             bExistDevice = true;
                             break;
                         }
                     }
+
                     if (!bExistDevice)
                     {
+                        // 前回のレコードに今回のデバイスIDが見つからなかったら、追加されたデバイスとみなす
                         string sTemp = string.Format("Add Device ID: {0}", usbDevice.DeviceID);
                         AddMessage(nowTime + sTemp + Environment.NewLine);
+                        AddListView(nowTime, "Add", usbDevice);
                     }
                 }
             }
             else if (usbDevices.Count < numBeforeUsbDevices)
             {
+                // 今回検出件数＜前回検出件数 ⇒ 削除
                 foreach (var usbDeviceBefore in usbDevicesBefore)
                 {
                     bool bExistDevice = false;
+
+                    // 今回検出したレコードと比較する
                     foreach (var usbDevice in usbDevices)
                     {
                         if (usbDeviceBefore.DeviceID == usbDevice.DeviceID)
                         {
+                            // 同一のデバイスIDが見つかれば、検索を中止する
                             bExistDevice = true;
                             break;
                         }
                     }
+
                     if (bExistDevice)
                     {
+                        // 今回のレコードに前回のレコードが見つからなければ、削除されたデバイスとみなす
                         string sTemp = string.Format("Del Device ID: {0}", usbDeviceBefore.DeviceID);
                         AddMessage(nowTime + sTemp + Environment.NewLine);
+                        AddListView(nowTime, "Del", usbDeviceBefore);
                     }
                 }
             }
             AddMessage(Environment.NewLine);        //可読性のため空行追加
             usbDevicesBefore = usbDevices;          //デバイス一覧を更新
             numBeforeUsbDevices = usbDevices.Count;
+            Console.WriteLine("終了時刻{0}", DateTime.Now.ToString("HH:mm:ss.ff "));
         }
 
         static List<USBDeviceInfo> GetUSBDevices()
@@ -84,12 +133,15 @@ namespace DetectUSB
 
             using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
                 collection = searcher.Get();
+
             foreach (var device in collection)
             {
                 devices.Add(new USBDeviceInfo(
+                    (string)device.GetPropertyValue("Caption"),
                     (string)device.GetPropertyValue("DeviceID"),
                     (string)device.GetPropertyValue("PNPDeviceID"),
-                    (string)device.GetPropertyValue("Description")
+                    (string)device.GetPropertyValue("Description"),
+                    (string)device.GetPropertyValue("Name")
                 ));
             }
             collection.Dispose();
@@ -113,22 +165,45 @@ namespace DetectUSB
                 this.Invoke(new Action<string>(AddMessage), msg);
             else
                 this.txtUSBInfo.AppendText(msg);
+
+                
+        }
+
+        public void AddListView(string tim, string name, USBDeviceInfo info)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<string, string, USBDeviceInfo>(AddListView), tim, name, info);
+            }
+            else
+            {
+                ListViewItem lvi;
+                lvi = lvwUsbDevice.Items.Add(tim);
+                lvi.SubItems.Add(name);
+                lvi.SubItems.Add(info.DeviceID);
+                lvi.SubItems.Add(info.Caption);
+                lvi.SubItems.Add(info.TestProp);
+            }
         }
 
     }
 
 
-    class USBDeviceInfo
+    public class USBDeviceInfo
     {
+        public string Caption { get; private set; }
         public string DeviceID { get; private set; }
         public string PnpDeviceID { get; private set; }
         public string Description { get; private set; }
+        public string TestProp { get; private set; }
 
-        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        public USBDeviceInfo(string caption, string deviceID, string pnpDeviceID, string description, string test)
         {
+            this.Caption = caption;
             this.DeviceID = deviceID;
             this.PnpDeviceID = pnpDeviceID;
             this.Description = description;
+            this.TestProp = test;
 
         }
     }
