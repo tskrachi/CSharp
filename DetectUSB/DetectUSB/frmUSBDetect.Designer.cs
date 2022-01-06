86d09d2f24fc165f8134821431b3581b0e6617c3
Tsunehiko Kurachi <tsk.rachi@gmail.com>
2022-01-06 14:53:29 +0900

    add UsbDetect solution

---
+++ DetectUSB/DetectUSB/frmUSBDetect.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DetectUSB
+{
+    partial class frmUSBDetect
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUSBInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUSBInfo
+            // 
+            this.txtUSBInfo.Location = new System.Drawing.Point(12, 80);
+            this.txtUSBInfo.Multiline = true;
+            this.txtUSBInfo.Name = "txtUSBInfo";
+            this.txtUSBInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUSBInfo.Size = new System.Drawing.Size(469, 79);
+            this.txtUSBInfo.TabIndex = 0;
+            // 
+            // frmUSBDetect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 213);
+            this.Controls.Add(this.txtUSBInfo);
+            this.Name = "frmUSBDetect";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUSBInfo;
+    }
+}
+
---
+++ DetectUSB/DetectUSB/frmUSBDetect.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Management;
+
+namespace DetectUSB
+{
+    public partial class frmUSBDetect : Form
+    {
+        public const int WM_DEVICECHANGE = 0x00000219;
+        // 
+        List<USBDeviceInfo> usbDevicesBefore = new List<USBDeviceInfo>();
+        int numBeforeUsbDevices = 0;
+
+        public frmUSBDetect()
+        {
+            InitializeComponent();
+            Task.Run(() => CheckDevice());
+        }
+                                                                                                                                                                                                                
+        private void CheckDevice()
+        {
+            var usbDevices = GetUSBDevices();
+            string nowTime = DateTime.Now.ToString("HH:mm:ss.ff ");
+
+            if (usbDevices.Count > numBeforeUsbDevices)
+            {
+                foreach( var usbDevice in usbDevices)
+                {
+                    bool bExistDevice = false;
+
+                    foreach (var usbDeviceBefore in usbDevicesBefore)
+                    {
+                        if (usbDevice.DeviceID == usbDeviceBefore.DeviceID)
+                        {
+                            bExistDevice = true;
+                            break;
+                        }
+                    }
+                    if (!bExistDevice)
+                    {
+                        string sTemp = string.Format("Add Device ID: {0}", usbDevice.DeviceID);
+                        AddMessage(nowTime + sTemp + Environment.NewLine);
+                    }
+                }
+            }
+            else if (usbDevices.Count < numBeforeUsbDevices)
+            {
+                foreach (var usbDeviceBefore in usbDevicesBefore)
+                {
+                    bool bExistDevice = false;
+                    foreach (var usbDevice in usbDevices)
+                    {
+                        if (usbDeviceBefore.DeviceID == usbDevice.DeviceID)
+                        {
+                            bExistDevice = true;
+                            break;
+                        }
+                    }
+                    if (bExistDevice)
+                    {
+                        string sTemp = string.Format("Del Device ID: {0}", usbDeviceBefore.DeviceID);
+                        AddMessage(nowTime + sTemp + Environment.NewLine);
+                    }
+                }
+            }
+            AddMessage(Environment.NewLine);        //可読性のため空行追加
+            usbDevicesBefore = usbDevices;          //デバイス一覧を更新
+            numBeforeUsbDevices = usbDevices.Count;
+        }
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+            // WMIを通じて取得される管理オブジェクト
+            ManagementObjectCollection collection;
+
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                    (string)device.GetPropertyValue("DeviceID"),
+                    (string)device.GetPropertyValue("PNPDeviceID"),
+                    (string)device.GetPropertyValue("Description")
+                ));
+            }
+            collection.Dispose();
+            return devices;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    Task.Run(() => CheckDevice());
+                    break;
+            }
+        }
+
+        public void AddMessage(string msg)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new Action<string>(AddMessage), msg);
+            else
+                this.txtUSBInfo.AppendText(msg);
+        }
+
+    }
+
+
+    class USBDeviceInfo
+    {
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+
+        }
+    }
+}
